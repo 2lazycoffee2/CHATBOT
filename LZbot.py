2fc3c6174f45584ecb1afb1f8d9191af9e51cf65
2lazycoffee2 <francisfrance255@gmail.com>
2025-07-18 20:12:52 +0200

    définition des entrées du chatbot

--- LZbot.py
+++ LZbot.py
@@ -1,9 +1,21 @@
 import spacy
 import nltk
 
+from nltk.tokenize import word_tokenize
+from nltk.tag import pos_tag
+
 #nltk.download('popular')
 
 # charger le model installé  :
+
+def preprocess(input_sentence) :
+    """
+    
+    """
+    words = word_tokenize(input_sentence)
+    pos_tags = pos_tag(words)
+    return pos_tags
+
 
 LZnlp = spacy.load('en_core_web_sm')
 
@@ -14,3 +26,10 @@ print([(w.text, w.pos_) for w in doc])
 doc1 = LZnlp("i love chatbots")
 
 print([(w.text, w.pos_) for w in doc1])
+
+
+# Définition des entrées : 
+
+input_sentence = input("entrez votre texte : ")
+processed_sentece = preprocess(input_sentence)
+print(processed_sentece)
