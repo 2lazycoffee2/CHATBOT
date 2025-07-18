599ac3f3b375f16e960b523c5f94244183be6b96
2lazycoffee2 <francisfrance255@gmail.com>
2025-07-18 21:12:01 +0200

    étape 2, reconnaissance des intentions

--- LZbot.py
+++ LZbot.py
@@ -1,35 +1,34 @@
 import spacy
 import nltk
 
-from nltk.tokenize import word_tokenize
-from nltk.tag import pos_tag
+from nltk.tokenize import word_tokenize # "tokenisation" découpage de  phrase en mots
+from nltk.tag import pos_tag            # POS tagging, identifie la grammaire des mots 
 
-#nltk.download('popular')
+#nltk.download('popular')                # ensemble de ressource populairee 
+#nltk.download('punkt')
+#nltk.download('averaged_perceptron_tagger_eng')
 
-# charger le model installé  :
 
 def preprocess(input_sentence) :
     """
-    
+    Tokenize une ohrase et, tag chaque mot selon
+    sa classe grammaticale et, retourne une liste 
+    de mot, et tag.
     """
     words = word_tokenize(input_sentence)
     pos_tags = pos_tag(words)
     return pos_tags
 
 
-LZnlp = spacy.load('en_core_web_sm')
-
-doc = LZnlp("this is a test")
-
-print([(w.text, w.pos_) for w in doc])
-
-doc1 = LZnlp("i love chatbots")
-
-print([(w.text, w.pos_) for w in doc1])
+#LZnlp = spacy.load('en_core_web_sm')    # Chargement du modèle en anglais pour les premiers tests
+#doc = LZnlp("this is a test")   
+#print([(w.text, w.pos_) for w in doc])  # Print chaque token.
+#doc1 = LZnlp("i love chatbots")
+#print([(w.text, w.pos_) for w in doc1])
 
 
 # Définition des entrées : 
 
 input_sentence = input("entrez votre texte : ")
-processed_sentece = preprocess(input_sentence)
-print(processed_sentece)
+processed_sentence = preprocess(input_sentence)
+print(processed_sentence)
