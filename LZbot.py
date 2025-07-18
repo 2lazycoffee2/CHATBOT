75570be22121b785caaba26c3a5ecfc5423772bc
2lazycoffee2 <francisfrance255@gmail.com>
2025-07-19 01:25:47 +0200

    Ajout de réponse au ChatBot

--- LZbot.py
+++ LZbot.py
@@ -21,14 +21,30 @@ def preprocess(input_sentence) :
 
 def recognize_intent(tokens):
     greeting_keywords = ['hello', 'hi', 'hey', 'greetings']
+    feeling_asking = ['how', 'are', 'you', 'okay', 'feel'] #?, 'how do you feel ?', 'are you okay ?', 'how are you', 'how do you feel', 'are you okay' ]
+    help_asking = ['give', 'me', 'help']
+    injures = ['ugly', 'dumb', 'idiot', 'son of a', 'useless']
+    
     tokens = [token.lower() for token, pos in tokens]
     if any(token in greeting_keywords for token in tokens):
         return "greeting"
+    elif any(token in feeling_asking for token in tokens):
+        return "feeling_asks"
+    elif any(token in help_asking for token in tokens):
+        return "asking_help"
+    elif any(token in injures for token in tokens):
+        return "injures"
     return "unknown"
 
 def generate_response(intent) : 
     if intent == "greeting":
         return "Hello ! How can i help you ?"
+    elif intent == "feeling_asks" :
+        return "Im feeling good today, teel me how can i help you ?"
+    elif intent == "asking_help" :
+        return "here is a list of what you can do : \nnothing... "
+    elif intent == "injures":
+        return "please, stay polite."
     else :
         return "sorry, i cannot understand for the moment..."
 
